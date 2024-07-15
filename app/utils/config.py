4eb7663a483a8254cfa1962eb78c615f21af011b
daniele <daniele.didino@gmail.com>
2024-07-15 12:54:23 +0200

    edit .env for heroku

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,9 +1,14 @@
 import environ
+import os
 
 # Initialize environment variables
 env = environ.Env()
-# Read .env file
-environ.Env.read_env('.env')
 
-# Access the OpenAI API key
-OPENAI_API_KEY = env("OPENAI_API_KEY")
+
+# Check for the presence of an environment variable first, then read .env file for local development
+if os.getenv("OPENAI_API_KEY"):
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+else:
+    # Initialize environment variables
+    environ.Env.read_env('.env')
+    OPENAI_API_KEY = env("OPENAI_API_KEY")
