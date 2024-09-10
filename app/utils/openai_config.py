acfb407fe961efaf2eb5826fa968376b0a4cdee3
daniele <daniele.didino@gmail.com>
2024-09-10 16:16:22 +0200

    change model to gpt4o

--- app/utils/openai_config.py
+++ app/utils/openai_config.py
@@ -3,7 +3,9 @@ from enum import Enum
 
 class OpenAIModels(str, Enum):
     GPT3_TURBO = "gpt-3.5-turbo"
-    GPT4 = "gpt-4"
+    GPT4 = "gpt-4",
+    GPT4o = "gpt-4o",
+    GPT4o_MINI = "gpt-4o-mini",
     TEXT_EMB_3_SMALL = "text-embedding-3-small"
 
 
